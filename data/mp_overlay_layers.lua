2df730ae9932d0d0b5e62b8d33cd88e6ee9d2f78
klandestino7 <klandestino6@hotmail.com>
2024-05-03 21:01:17 -0300

    upload components e overlays raw data

---
+++ data/mp_overlay_layers.lua
@@ -0,0 +1,922 @@
+gMpOverlayLayers = {
+	{
+		type = "MPC_OVERLAY_LAYER_EYEBROWS",
+		styles = {
+			{
+				metapedType = "MALE",
+				albedo = "0xf9052779",
+				normal = "0x21bb2d97",
+				material = "0x75a0b928"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xf3351bd9",
+				normal = "0x8bb9158a",
+				material = "0xfbbae4d8"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x216ef84c",
+				normal = "0x2f54c727",
+				material = "0xccbd1939"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x139a5ca3",
+				normal = "0x22a9ddf9",
+				material = "0x78aa9401"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xfe183197",
+				normal = "0x6aa92a3e",
+				material = "0xb4a436db"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xf0ca96fc",
+				normal = "0x667feff8",
+				material = "0xdd8e5eff"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xa83ca6e",
+				normal = "0xbd811948",
+				material = "0xb82c8fbb"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xbd38afd9",
+				normal = "0x9a732f86",
+				material = "0x2ef1d769"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xa6de8325",
+				normal = "0x22b33b65",
+				material = "0xee6ccf11"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xcd0a4f7c",
+				normal = "0xb5b73a38",
+				material = "0x15c5fb78"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xeb088a20",
+				normal = "0x91a2496e",
+				material = "0xe639f138"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x1832e474",
+				normal = "0x32fa2683",
+				material = "0xa1775b18"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x7844317",
+				normal = "0x7bc4288b",
+				material = "0x202674a1"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x33c39bc5",
+				normal = "0x46c268bd",
+				material = "0x4b92f13e"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x443e3cba",
+				normal = "0x2b191070",
+				material = "0xd551e623"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x4f5052de",
+				normal = "0x70e8c702",
+				material = "0xd97518f9"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xd0ec86ff",
+				normal = "0x894f8744",
+				material = "0x51551810"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x5c049d35",
+				normal = "0x7e47d163",
+				material = "0x54100288"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x8a4b79c2",
+				normal = "0x89b29e5a",
+				material = "0xfa0476e4"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x77a1546e",
+				normal = "0x290fc7f7",
+				material = "0xd8fc26a9"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x2594304d",
+				normal = "0x8611b42c",
+				material = "0x238302b"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xa8ccb6c4",
+				normal = "0x34abb09d",
+				material = "0xcf206860"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x9728137b",
+				normal = "0xee39073f",
+				material = "0x218dd4c8"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xb3f74d19",
+				normal = "0xd4809d11",
+				material = "0x9abfa640"
+			}
+		}
+	},
+	{
+		type = "MPC_OVERLAY_LAYER_SCAR",
+		styles = {
+			{
+				metapedType = "MALE",
+				albedo = "0xc8e45b5b",
+				normal = "0xd53a336f"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x90d86b44",
+				normal = "0xdfcb1159"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x23190fc3",
+				normal = "0x249c1a0a"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x7574b47d",
+				normal = "0x7a70886a"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x7fe8c965",
+				normal = "0x7210971b"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x83059fe",
+				normal = "0x860ee45e"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x19e9fd71",
+				normal = "0xb753c5c7"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x4caf62fb",
+				normal = "0xbbf225"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xde650668",
+				normal = "0x3dd0b0ae"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xc648562b",
+				normal = "0x2b59cda1"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x484baef8",
+				normal = "0xd3f2f2f6"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x190f5080",
+				normal = "0x9518fa34"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x2b5df51d",
+				normal = "0x8b8c57ac"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xe490e784",
+				normal = "0xda7f2a1e"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xed23c06",
+				normal = "0x364daaa6"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x5712ccb6",
+				normal = "0x98104c8c"
+			}
+		}
+	},
+	{
+		type = "MPC_OVERLAY_LAYER_EYELINER",
+		styles = {
+			{
+				metapedType = "MALE",
+				albedo = "0x29a2e58f",
+				material = "0xdd55af2a"
+			}
+		}
+	},
+	{
+		type = "MPC_OVERLAY_LAYER_LIPSTICK",
+		styles = {
+			{
+				metapedType = "FEMALE",
+				albedo = "0x887e11e0",
+				normal = "0x1c77591c",
+				material = "0x4255a5f4"
+			}
+		}
+	},
+	{
+		type = "ACNE",
+		styles = {
+			{
+				metapedType = "MALE",
+				albedo = "0x96dd8f42",
+				normal = "0xba46ce92"
+			}
+		}
+	},
+	{
+		type = "MPC_OVERLAY_LAYER_EYESHADOW",
+		styles = {
+			{
+				metapedType = "FEMALE",
+				albedo = "0x47bd7289",
+				material = "0xe20345cc"
+			}
+		}
+	},
+	{
+		type = "MPC_OVERLAY_LAYER_FACIAL_HAIR",
+		styles = {
+			{
+				metapedType = "MALE",
+				albedo = "0x375d4807",
+				normal = "0x5041b648",
+				material = "0x83f42340"
+			}
+		}
+	},
+	{
+		type = "MPC_OVERLAY_LAYER_FACE_PAINT",
+		styles = {
+			{
+				metapedType = "MALE",
+				albedo = "0x5995aa6f"
+			}
+		}
+	},
+	{
+		type = "MPC_OVERLAY_LAYER_AGEING",
+		styles = {
+			{
+				metapedType = "MALE",
+				albedo = "0x96dd8f42",
+				normal = "0xba46ce92"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x6d9dc405",
+				normal = "0x5cf8808e"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x2761b792",
+				normal = "0x8607cc56"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x19009ad0",
+				normal = "0x9087af96"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xc29f6e07",
+				normal = "0x1331c3c9"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xa45f3187",
+				normal = "0x3ca2f3ae"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x5e21250c",
+				normal = "0x5a965ff0"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x4ffe08c6",
+				normal = "0xc46cc005"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x2dad4485",
+				normal = "0x55d317b4"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x3f70680b",
+				normal = "0x33e73c5f"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xd3310f8e",
+				normal = "0x22297ea5"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xf27a4c84",
+				normal = "0x9f0e6718"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x44e819",
+				normal = "0x315a6d56"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xa648348d",
+				normal = "0xe8cd7f20"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x94f991f0",
+				normal = "0xca334396"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xcaacfd56",
+				normal = "0xe0203bda"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xb9675acb",
+				normal = "0x90a80ae1"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x3c2ce03c",
+				normal = "0x609b7ebd"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xf2d64d90",
+				normal = "0x609b7ebd"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xe389aef7",
+				normal = "0x11d92a14"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x89317a44",
+				normal = "0x2f56fda5"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x64b3347c",
+				normal = "0x45ee7b10"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x9ffdab10",
+				normal = "0x85bdd7e8"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x91d40ebd",
+				normal = "0xa1b5f71f"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x6b94c23f",
+				normal = "0xc480977"
+			}
+		}
+	},
+	{
+		type = "MPC_OVERLAY_LAYER_BLUSHER",
+		styles = {
+			{
+				metapedType = "FEMALE",
+				albedo = "0x6db440fa"
+			},
+			{
+				metapedType = "FEMALE",
+				albedo = "0x47617455"
+			},
+			{
+				metapedType = "FEMALE",
+				albedo = "0x114d082d"
+			},
+			{
+				metapedType = "FEMALE",
+				albedo = "0xec6f3e72"
+			}
+		}
+	},
+	{
+		type = "MPC_OVERLAY_LAYER_COMPLEXION",
+		styles = {
+			{
+				metapedType = "MALE",
+				albedo = "0xf679ede7"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x3ffb80ed"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x31c0e478"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x2457c9a6"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x16262d43"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x88f312db"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x785c71ae"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x6d7d5bf0"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x5f2fbf55"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xbf38ff6a"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xf5656c26"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x3a408a3"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x293453c3"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x43150800"
+			}
+		}
+	},
+	{
+		type = "MPC_OVERLAY_LAYER_SKIN_MOTTLING",
+		styles = {
+			{
+				metapedType = "MALE",
+				albedo = "0xd44a5aba"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xe2cf77c4"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xcf57d0e9"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xe0a8738a"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xabd109dc"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xb91c2472"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x894844b7"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x96fae01c"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x86d3bfce"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x5488db39"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x7da5a5ae"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xe73778dc"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xd83edadf"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xe380f163"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xb4611324"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xc6abb7b9"
+			}
+		}
+	},
+	{
+		type = "MPC_OVERLAY_LAYER_FOUNDATION",
+		styles = {
+			{
+				metapedType = "FEMALE",
+				albedo = "0xef5ab280",
+				material = "0x1535c7c9"
+			}
+		}
+	},
+	{
+		type = "MPC_OVERLAY_LAYER_FRECKLES",
+		styles = {
+			{
+				metapedType = "MALE",
+				albedo = "0x1b794c51"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x29bfe8de"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xef6b34c"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x64925e7e"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xf5f280fc"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x33b0fc78"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x25675fe5"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xd10f3736"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x5126b75f"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x6b8eec2f"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xa9a26f7"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xfde40d8b"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x7e338e44"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x70f273c2"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x61c7d56d"
+			}
+		}
+	},
+	{
+		type = "MPC_OVERLAY_LAYER_GRIME",
+		styles = {
+			{
+				metapedType = "MALE",
+				albedo = "0xa2f30923",
+				normal = "0x136165b3",
+				material = "0xf3dfa7ac"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xd5b1eea0",
+				normal = "0x5c67fb68",
+				material = "0x40fec59e"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x7ec740cc",
+				normal = "0x9e7a4b63",
+				material = "0xb48bf65a"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xb08f245b",
+				normal = "0x1faa4a84",
+				material = "0x81428e8f"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x1a5e77f8",
+				normal = "0x1faa4a84",
+				material = "0x81428e8f"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xe81b9373",
+				normal = "0xcbeef9b",
+				material = "0x92097b22"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x3cfa3d2f",
+				normal = "0xa27ff667",
+				material = "0x24b49749"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xb865a48",
+				normal = "0x377319e3",
+				material = "0x3cdc25a9"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x506de416",
+				normal = "0x6af092",
+				material = "0x5ccea9f8"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x1f250185",
+				normal = "0x3f718027",
+				material = "0x5527accf"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xe71930b0",
+				normal = "0xf4e08d43",
+				material = "0x60b91ce7"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xde571f2c",
+				normal = "0xe6a18bbf",
+				material = "0xcb315a57"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xca6fbcb",
+				normal = "0xd4894921",
+				material = "0xbf339d56"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x21f62669",
+				normal = "0xdb95176c",
+				material = "0xea27b375"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xfb09d881",
+				normal = "0xadd1dc3d",
+				material = "0xfd797a87"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x11530513",
+				normal = "0x26aa38c3",
+				material = "0x89c2ffe3"
+			}
+		}
+	},
+	{
+		type = "MPC_OVERLAY_LAYER_HEAD_HAIR",
+		styles = {
+			{
+				metapedType = "MALE",
+				albedo = "0x39051515",
+				normal = "0x8d65eff2",
+				material = "0x62759d82"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x5e71dfee",
+				material = "0xd8eb57bc"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xdd735def",
+				material = "0x6613d121"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x69622ead",
+				material = "0xe581d851"
+			}
+		}
+	},
+	{
+		type = "MPC_OVERLAY_LAYER_MOLES",
+		styles = {
+			{
+				metapedType = "MALE",
+				albedo = "0x821fd077"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xcd38e6a8"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x9f9d8b72"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xe7179a39"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xbb094249"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x3ac5362"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x154ff6a9"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x1e23084f"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x31dbafc0"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x3ac5c194"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x4500d516"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x3695b840"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x286c1bed"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x934bf1af"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x84f55502"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xbd9a464b"
+			}
+		}
+	},
+	{
+		type = "MPC_OVERLAY_LAYER_SPOTS",
+		styles = {
+			{
+				metapedType = "MALE",
+				albedo = "0x5bbff5f7"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x65ec0a4f"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x3f143ca0"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x49675146"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0x7504d2d"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xf161214f"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xe43286f2"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xdddc7a46"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xd086df9b"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xba51b331"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xe4cf097b"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xf70cadf6"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xc07f40dc"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xd3b1e741"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xb494a903"
+			},
+			{
+				metapedType = "MALE",
+				albedo = "0xc6ee4db6"
+			}
+		}
+	}
+}
+
+exports('mp_overlay_layers', function()
+    return gMpOverlayLayers
+end)

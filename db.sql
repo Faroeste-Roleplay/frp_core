b14391fae0e679c9981922a1c67db7f90b4b8449
klandestino7 <klandestino6@hotmail.com>
2024-05-08 13:48:17 -0300

    fix sql file

--- db.sql
+++ db.sql
@@ -207,7 +207,7 @@ CREATE TABLE IF NOT EXISTS `group_member` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `groupId` int(11) DEFAULT NULL,
   `userId` int(11) NOT NULL,
-  `personaId` int(11) DEFAULT NULL,
+  `characterId` int(11) DEFAULT NULL,
   `createdAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;

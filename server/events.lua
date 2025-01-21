b433d2ed3b3264c569812703c6e925720b0f4704
klandestino7 <klandestino6@hotmail.com>
2025-01-21 09:19:36 -0300

    feat(server/events): add dimension event

--- server/events.lua
+++ server/events.lua
@@ -35,3 +35,42 @@ end)
 RegisterNetEvent('FRP:sv_playAmbSpeech', function(pedNet, line)
     TriggerClientEvent('FRP:cl_onPlayAmbSpeech', -1, pedNet, line)
 end)
+
+AddEventHandler("net.session.requestEnterDimension", function(playerId, dimensionId, transportEntityNetworkId)
+    -- Garantir que a dimensão corresponde ao jogador
+    if tonumber(dimensionId) ~= tonumber(playerId) then
+        return
+    end
+
+    -- Se um transportEntityNetworkId for fornecido, ajustar sua dimensão
+    if transportEntityNetworkId then
+        local transportEntityId = NetworkGetEntityFromNetworkId(transportEntityNetworkId)
+
+        if transportEntityId then
+            SetEntityRoutingBucket(transportEntityId, dimensionId)
+        end
+    end
+
+    -- Ajustar a dimensão do jogador
+    VirtualWorld:AddPlayerOnVirtualWorld(playerId, dimensionId)
+end)
+
+
+AddEventHandler("net.session.requestEnterDimension", function(playerId, dimensionId, transportEntityNetworkId)
+    -- Garantir que o jogador está em uma dimensão (não na padrão)
+    if GetPlayerRoutingBucket(playerId) == 0 then
+        return
+    end
+
+    -- Se um transportEntityNetworkId for fornecido, ajustar sua dimensão de volta para 0
+    if transportEntityNetworkId then
+        local transportEntityId = NetworkGetEntityFromNetworkId(transportEntityNetworkId)
+
+        if transportEntityId then
+            SetEntityRoutingBucket(transportEntityId, 0)
+        end
+    end
+
+    -- Ajustar a dimensão do jogador de volta para 0
+    VirtualWorld:AddPlayerOnVirtualWorld(playerId, 0)
+end)

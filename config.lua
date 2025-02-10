aad588a4f15c1ee25e5e4f6dac9b1b7e91654934
klandestino7 <klandestino6@hotmail.com>
2025-02-10 15:52:15 -0300

    tweak(config): update default spawn coords + LoggedInDiscordRole

--- config.lua
+++ config.lua
@@ -7,11 +7,22 @@ Config.SkyCamSpawnEffect = false
 Config.DefaultSpawnPosition = vector3(-1099.470, -1839.129, 60.327)
 Config.DisableAutoSpawn = true
 
+-- in-game role
+Config.LoggedInDiscordRole = 1338513326656979025
+
+-- Config.DefaultFirstSpawnCoords = { -- saint denis
+--     vector3(2695.72, -1444.99, 45.30),
+--     vector3(2696.68, -1447.11, 45.30),
+--     vector3(2698.41, -1450.62, 45.30),
+--     vector3(2699.35, -1453.15, 45.30)
+-- }
+
 Config.DefaultFirstSpawnCoords = {
-    vector3(2695.72, -1444.99, 45.30),
-    vector3(2696.68, -1447.11, 45.30),
-    vector3(2698.41, -1450.62, 45.30),
-    vector3(2699.35, -1453.15, 45.30)
+    vector3(-302.92, 782.45, 117.78),
+    vector3(-332.32, 776.93, 116.44),
+    vector3(-164.59, 634.97, 113.08),
+    vector3(-171.1, 625.68, 113.08),
+    vector3(-171.72, 630.57, 113.08)
 }
 
 -- [[ steam or discord or fivem]]

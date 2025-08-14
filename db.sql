4c33c2e952db638385405b6eb1cb502295928421
klandestino7 <klandestino6@hotmail.com>
2025-08-14 13:23:18 -0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,23 +1,3 @@
--- --------------------------------------------------------
--- Servidor:                     127.0.0.1
--- Versão do servidor:           11.1.2-MariaDB - mariadb.org binary distribution
--- OS do Servidor:               Win64
--- HeidiSQL Versão:              12.5.0.6677
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
--- Copiando estrutura do banco de dados para faroeste
-CREATE DATABASE IF NOT EXISTS `frp` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
-USE `frp`;
-
 -- Copiando estrutura para tabela faroeste.ban_batch
 CREATE TABLE IF NOT EXISTS `ban_batch` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
